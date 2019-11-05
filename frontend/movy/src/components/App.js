985743e8229c23fedda4273ea94a93134403b991
Johnny Zavala <jzavalafgz@gmail.com>
2019-11-05 16:24:09 -0500

    [frontend] [src] Added components to Home page

--- frontend/movy/src/components/App.js
+++ frontend/movy/src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Home from './Home';
 import Header from './elements/Header';
 
-const App = () => <div><Header /></div>;
+const App = () => (
+    <div>
+        <Header />
+        <Home />
+    </div>
+);
 
 export default App;
---
+++ frontend/movy/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import HeroImage from './elements/HeroImage'
+import SearchBar from './elements/SearchBar'
+import Grid from './elements/Grid'
+import MovieThumb from './elements/MovieThumb'
+import Spinner from './elements/Spinner'
+import LoadMoreBtn from './elements/LoadMoreBtn'
+
+const Home = () => (
+    <>
+        <HeroImage />
+        <SearchBar />
+        <Grid />
+        <MovieThumb />
+        <Spinner />
+        <LoadMoreBtn />
+    </>
+)
+
+export default Home;
