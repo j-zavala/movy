35efc41901bdb91cfaa72eab952dc8d91b41f503
Johnny Zavala <jzavalafgz@gmail.com>
2019-11-11 20:21:10 -0500

    Fixed error when img tag was incorrectly labled image

--- client/movy/src/components/elements/MovieThumb.js
+++ client/movy/src/components/elements/MovieThumb.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import { StyledMovieThumb } from '../styles/StyledMovieThumb'
 
-const MovieThumb = () => <div>MovieThumb</div>
+const MovieThumb = ({ image, movieId, clickable }) => (
+    <StyledMovieThumb>
+        {clickable ? (
+            <img className="clickable" src={image} alt="moviethumb" />
+        ) : (
+                <img src={image} alt="moviethumb" />
+            )
+        }
+    </StyledMovieThumb>
+)
 
 export default MovieThumb;
