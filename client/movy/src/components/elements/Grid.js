d101e124182f92b6555e9a92fcea25f574d3be2e
Johnny Zavala <jzavalafgz@gmail.com>
2019-11-11 19:49:08 -0500

    [frontend] [src] Added content to Grid.js using props

--- client/movy/src/components/elements/Grid.js
+++ client/movy/src/components/elements/Grid.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import { StyledGrid, StyledGridContent } from '../styles/StyledGrid'
 
-const Grid = () => <div>Grid</div>
+//children prop is going to be auto-created for us; grabs the components nested inside the of the component we're in
+//so children is going to contain all of the props mapped out in MovieThumb in Home.js
+const Grid = ({ header, children }) => (
+    <StyledGrid>
+        <h1>{header}</h1>
+        <StyledGridContent>{children}</StyledGridContent>
+    </StyledGrid>)
 
 export default Grid;
