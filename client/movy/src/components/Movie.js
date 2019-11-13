1adcde881c7971d3f4dd6b6ec005db1be27f60de
Johnny Zavala <jzavalafgz@gmail.com>
2019-11-13 18:43:53 -0500

    outlined components in Movie.js

--- client/movy/src/components/Movie.js
+++ client/movy/src/components/Movie.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import Navigation from './elements/Navigation';
+import MovieInfo from './elements/MovieInfo';
+import MovieInfoBar from './elements/MovieInfoBar';
+import Grid from './elements/Grid';
+import Actor from './elements/Actor';
+import Spinner from './elements/Spinner';
 
-const Movie = ({ movieId }) => <div>Movie: {movieId}</div>
+//Components
+
+
+const Movie = ({ movieId }) => (
+    <>
+        <Navigation />
+        <MovieInfo />
+        <MovieInfoBar />
+        <Grid>
+            <Actor />
+        </Grid>
+        <Spinner />
+    </>
+)
 
 export default Movie;
